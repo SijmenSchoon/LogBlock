32f176b84061c3c75428e276338b4f17b6a49637
Sijmen Schoon <me@sijmenschoon.nl>
2019-11-28 13:20:20 +0100

    Fix compiler errors and some more refactoring

--- src/main/java/de/diddiz/LogBlock/listeners/ChestAccessLogging.java
+++ src/main/java/de/diddiz/LogBlock/listeners/ChestAccessLogging.java
@@ -12,11 +12,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import static de.diddiz.LogBlock.config.Config.isLogging;
 import static de.diddiz.util.BukkitUtils.*;
@@ -31,56 +34,62 @@ public class ChestAccessLogging extends LoggingListener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onInventoryClose(InventoryCloseEvent event) {
-        if (!isLogging(event.getPlayer().getWorld(), Logging.CHESTACCESS)) {
+        final HumanEntity player = event.getPlayer();
+        if (!isLogging(player.getWorld(), Logging.CHESTACCESS)) {
             return;
         }
 
-        InventoryHolder holder = event.getInventory().getHolder();
-        if (holder instanceof BlockState || holder instanceof DoubleChest) {
-            final HumanEntity player = event.getPlayer();
-            final ItemStack[] before = containers.remove(player);
-            if (before == null) {
-                logger.log(Level.VERBOSE, "Player closed inventory without first opening it, skipping.");
-                return;
-            }
+        final Inventory inventory = event.getInventory();
+        final InventoryHolder holder = inventory.getHolder();
+        if (!(holder instanceof BlockState || holder instanceof DoubleChest)) {
+            return;
+        }
 
-            final ItemStack[] after = compressInventory(event.getInventory().getContents());
-            final ItemStack[] diff = compareInventories(before, after);
-            
-            final Location chestLocation = getInventoryHolderLocation(holder);
-            if (chestLocation == null) {
-                logger.log(Level.VERBOSE, "Unknown chest location when closing inventory, skipping.");
-                return;
-            }
+        final ItemStack[] before = containers.remove(player);
+        if (before == null) {
+            logger.log(Level.INFO, "Player closed inventory without first opening it, skipping.");
+            return;
+        }
 
-            for (final ItemStack stack : diff) {
-                ItemStack stackAbs = stack.clone();
-                stackAbs.setAmount(Math.abs(stack.getAmount()));
- 
-                consumer.queueChestAccess(
-                    Actor.actorFromEntity(player),
-                    chestLocation,
-                    chestLocation.getWorld().getBlockAt(chestLocation).getBlockData(),
-                    stackAbs,
-                    stack.getAmount() < 0
-                );
-            }
+        final ItemStack[] after = compressInventory(inventory.getContents());
+        final ItemStack[] diff = compareInventories(before, after);
+
+        final Location chestLocation = getInventoryHolderLocation(holder);
+        if (chestLocation == null) {
+            logger.log(Level.INFO, "Unknown chest location when closing inventory, skipping.");
+            return;
+        }
+
+        for (final ItemStack stack : diff) {
+            final ItemStack stackAbs = stack.clone();
+            stackAbs.setAmount(Math.abs(stack.getAmount()));
+
+            consumer.queueChestAccess(
+                Actor.actorFromEntity(player),
+                chestLocation,
+                chestLocation.getWorld().getBlockAt(chestLocation).getBlockData(),
+                stackAbs,
+                stack.getAmount() < 0
+            );
         }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onInventoryOpen(InventoryOpenEvent event) {
-
-        if (!isLogging(event.getPlayer().getWorld(), Logging.CHESTACCESS)) {
+        final HumanEntity player = event.getPlayer();
+        if (!isLogging(player.getWorld(), Logging.CHESTACCESS)) {
             return;
         }
-        if (event.getInventory() != null) {
-            InventoryHolder holder = event.getInventory().getHolder();
-            if (holder instanceof BlockState || holder instanceof DoubleChest) {
-                if (getInventoryHolderType(holder) != Material.CRAFTING_TABLE) {
-                    containers.put(event.getPlayer(), compressInventory(event.getInventory().getContents()));
-                }
-            }
+
+        final Inventory inventory = event.getInventory();
+        if (inventory == null) {
+            return;
+        }
+
+        final InventoryHolder holder = event.getInventory().getHolder();
+        if ((holder instanceof BlockState || holder instanceof DoubleChest) &&
+                getInventoryHolderType(holder) != Material.CRAFTING_TABLE) {
+            containers.put(player, compressInventory(inventory.getContents()));
         }
     }
 }
